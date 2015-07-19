6104e3b637d8745b5a29b7de6b37d408bef19a3b
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-07-19 01:52:14 -0400

    Update mesh.lua, torus.lua

--- data/mesh.lua
+++ data/mesh.lua
@@ -8,12 +8,14 @@ require('readobj')
 -- Scene root
 scene = gr.node('scene')
 
-radius = 0.7
+radius = 0.88
 length = 10
 
---teapot = gr.mesh('teapot', readobj('objs/teapot_n.obj'))
---scene:add_child(teapot)
---teapot:set_material(white_cornell)
+teapot = gr.mesh('teapot', readobj('objs/teapot_n.obj'))
+scene:add_child(teapot)
+teapot:set_material(white_cornell)
+teapot:scale(radius, radius, radius)
+teapot:translate(-0.1, 0, -3.5)
 
 --torus = gr.torus('torus')
 --scene:add_child(torus)
@@ -30,13 +32,6 @@ floor:set_texture('textures/boards02.png')
 floor:set_bumpmap('bumps/boards02-bump.png', 0.005)
 floor:scale(length, 1.0, length)
 
-backwall = gr.plane('backwall')
-scene:add_child(backwall)
-backwall:set_material(white_cornell)
-backwall:translate(0, 0, 2)
-backwall:rotate('x', -70)
-backwall:scale(length, 1.0, length)
-
 -- lights
 light_color = {0.780131, 0.780409, 0.775833}
 light_color_2 = {0.780131/2, 0.780409/2, 0.775833/2}
@@ -44,10 +39,11 @@ light_color = {1.0, 1.0, 1.0}
 
 -- on ceiling
 light1 = gr.disc_light({10, 10, 0}, light_color, {1, 0, 0}, {0, -1, 0}, 2)
+light2 = gr.disc_light({0, 0, -4}, light_color_2, {1, 0, 0}, {0, 0, 1}, 2)
 
 gr.render(scene,
 	  'mesh.png', 512, 512,
-	  {0, 4, -4.5}, {0, -7.871199, 4.612095}, {0, 1, 0}, 50,
-	  {0.1,0.1,0.1}, {light1},
+	  {0, 2, -4.5}, {0, -5.871199, 4.612095}, {0, 1, 0}, 50,
+	  {0.1,0.1,0.1}, {light1, light2},
     4, 4, 2, 1)
 
