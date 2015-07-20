8b6df366bbf68eb948648e5eb041a8bc777d4621
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-07-20 00:09:05 -0400

    Update mesh.lua

--- data/mesh.lua
+++ data/mesh.lua
@@ -8,14 +8,16 @@ require('readobj')
 -- Scene root
 scene = gr.node('scene')
 
-radius = 0.88
+radius = 0.58
 length = 10
+
+white = gr.material({1.0, 1.0, 1.0}, {0, 0, 0}, 5)
 
 teapot = gr.tri_mesh('teapot', readobj('objs/teapot_n.obj'))
 scene:add_child(teapot)
-teapot:set_material(white_cornell)
+teapot:set_material(white)
+teapot:translate(-0.05, 0, -3.1)
 teapot:scale(radius, radius, radius)
-teapot:translate(-0.1, 0, -3.5)
 
 --torus = gr.torus('torus')
 --scene:add_child(torus)
@@ -35,7 +37,7 @@ floor:scale(length, 1.0, length)
 -- lights
 light_color = {0.780131, 0.780409, 0.775833}
 light_color_2 = {0.780131/2, 0.780409/2, 0.775833/2}
-light_color = {1.0, 1.0, 1.0}
+light_color_3 = {1.0, 1.0, 1.0}
 
 -- on ceiling
 light1 = gr.disc_light({5, 10, -10}, light_color, {1, 0, 0}, {0, -1, 0}, 2)
@@ -43,7 +45,7 @@ light1 = gr.disc_light({5, 10, -10}, light_color, {1, 0, 0}, {0, -1, 0}, 2)
 
 gr.render(scene,
 	  'mesh.png', 512, 512,
-	  {0, 2, -4.5}, {0, -5.871199, 4.612095}, {0, 1, 0}, 50,
+	  {0, 1, -4.5}, {0, -2.871199, 4.612095}, {0, 1, 0}, 50,
 	  {0.1,0.1,0.1}, {light1},
-    4, 4, 2, 1)
+    4, 4, 4, 64)
 
