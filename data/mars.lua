025ea361fea7b313bf80203b60e9788c3e131f78
Sekhar Bhattacharya <sekhar.bx@gmail.com>
2015-07-20 23:13:57 -0400

    Update mars.lua and texture.lua with disc lights

--- data/mars.lua
+++ data/mars.lua
@@ -21,19 +21,19 @@ light_color = {0.780131, 0.780409, 0.775833}
 light_color_2 = {0.780131/2, 0.780409/2, 0.775833/2}
 light_color = {1.0, 1.0, 1.0}
 
-light1 = gr.light({10, 10, 0}, light_color, {1, 0, 0})
-light2 = gr.light({10, 10, 0}, light_color, {1, 0, 0})
+light1 = gr.disc_light({10, 10, 0}, light_color, {1, 0, 0}, {-10, -10, 15}, 1)
+light2 = gr.disc_light({10, 10, 0}, light_color, {1, 0, 0}, {-10, -10, 15}, 1)
 
 gr.render(scene,
 	  'mars.png', 512, 512,
 	  {0, 0, -1}, {0, 0, 1}, {0, 1, 0}, 50,
 	  {0.1,0.1,0.1}, {light1, light2},
-    4, 4, 2, 1, 'backgrounds/starry.sky.png')
+    4, 4, 4, 1, 'backgrounds/starry.sky.png')
 
 -- wide
 --gr.render(scene,
 --	  'mars.2048.512.png', 2048, 512,
 --	  {0, 0, -1}, {0, 0, 1}, {0, 1, 0}, 30,
 --	  {0.1,0.1,0.1}, {light1, light2},
---    4, 4, 3, 1, 'backgrounds/starry.sky.png')
+--    4, 4, 4, 1, 'backgrounds/starry.sky.png')
 
